aa211ecede17b036d8f7c933777239bc158ac6e1
Minh Lam <lamworkspace74@gmail.com>
2026-01-07 13:49:26 +0700

    rename folder

---
+++ frontend/src/app/pages/order/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+import OrderPage from '@/features/orders/pages/OrderPage';
+
+// This will throw an error
+export default function Page() {
+    return <OrderPage />
+}
---
+++ frontend/src/features/orders/pages/OrderPage.tsx
@@ -0,0 +1,8 @@
+import { Title } from '@mantine/core'
+import React from 'react'
+
+export default function OrderPage() {
+    return (
+        <Title>This is order page</Title>
+    )
+}
