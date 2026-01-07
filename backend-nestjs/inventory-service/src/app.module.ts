9803710d0ede86dcd8ed5747fa535af21aca7e91
Minh Lam <lamworkspace74@gmail.com>
2026-01-07 17:12:02 +0700

    feat: add environment variable validation using Joi for inventory and orders services

--- backend-nestjs/inventory-service/src/app.module.ts
+++ backend-nestjs/inventory-service/src/app.module.ts
@@ -5,11 +5,19 @@ import { ConfigModule } from '@nestjs/config';
 import { ReserveModule } from '@/modules/reserve.module';
 import { InventoryModule } from '@/modules/inventory.module';
 import { PrismaModule } from '@/prisma/prisma.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 👈 important
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3002),
+        DATABASE_URL: Joi.string().required(),
+      }),
     }),
     InventoryModule,
     ReserveModule,
--- backend-nestjs/orders-service/src/app.module.ts
+++ backend-nestjs/orders-service/src/app.module.ts
@@ -4,11 +4,20 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { OrdersModule } from '@/modules/order.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 👈 important
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3001),
+        INVENTORY_BASE_URL: Joi.string().uri().required(),
+        INVENTORY_TIMEOUT_MS: Joi.number().default(2000),
+      }),
     }),
     OrdersModule,
     HttpModule,
